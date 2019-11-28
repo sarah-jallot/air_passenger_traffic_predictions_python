520b894f6a402bfc81cec8511d321b92f25dbe18
Sarah Jallot <sarah.jallot@hec.edu>
2019-11-28 17:22:54 +0100

    Adding new folders and files

---
+++ air_passengers/submissions/sarvic_2711_1248_data/regressor.py
@@ -0,0 +1,23 @@
+# Basic combined regressor 
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import GradientBoostingRegressor
+
+class Regressor(BaseEstimator):
+    def __init__(self):
+        self.reg = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
+                           learning_rate=0.1, loss='ls', max_depth=5,
+                           max_features=None, max_leaf_nodes=None,
+                           min_impurity_decrease=0.0, min_impurity_split=None,
+                           min_samples_leaf=5, min_samples_split=10,
+                           min_weight_fraction_leaf=0.0, n_estimators=300,
+                           n_iter_no_change=None, presort='auto',
+                           random_state=None, subsample=1.0, tol=0.0001,
+                           validation_fraction=0.1, verbose=0, warm_start=False)
+
+    def fit(self, X, y):
+        self.reg.fit(X,y)
+
+
+    def predict(self, X):
+        return self.reg.predict(X)
